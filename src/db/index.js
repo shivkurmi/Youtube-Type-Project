62768185174fdc10a2257efc7f3cf7ac9679b631
shiv chaudhary <shivkurmi1327@gmail.com>
2025-05-11 09:04:36 +0530

    update files info

--- src/db/index.js
+++ src/db/index.js
@@ -4,14 +4,14 @@ import { db_Name } from "../contants.js";
 const dataBaseConnection=async ()=>{
   try{
     const connect= await mongoose.connect(`${process.env.MONGODB_URI}/${db_Name}`)
-    console.log("Database connected");
     
     console.log("Mongodb connection is host ",connect.connection.host);
   }
   catch(err){
     console.log("error",err);
+    
     process.exit(1)
   }
 }
 
-export default dataBaseConnection;
+export default dataBaseConnection
