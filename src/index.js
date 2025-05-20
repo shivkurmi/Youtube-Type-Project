624b44c3961ad88b1db40b0413efea4592e22525
shiv chaudhary <shivkurmi1327@gmail.com>
2025-05-20 08:18:13 +0530

    Added new utils files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from 'dotenv'
 import dataBaseConnection from './db/index.js'
+import { app } from './app.js';
 
 dotenv.config({
   host:'./env'
 })
 dataBaseConnection()
+.then(()=>{
+  app.listen(process.env.PORT || 3000,()=>{
+    console.log(`Servering is runninng on port ${process.env.PORT}`);
+    
+  })
+})
+.catch((err)=>{
+  console.log("Mongodb connnection failed ",err);
+  
+})
 
 
 
